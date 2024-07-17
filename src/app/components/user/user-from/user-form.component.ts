991e788e4a6fa2116d2b3acd55b4f4fed2b1fcec
Ignacio Zelada <109553320+IgnacioZAraya@users.noreply.github.com>
2024-07-17 16:42:21 -0600

    Toastr Alert Implementation
    Co-authored-by: DeibiU <DeibiU@users.noreply.github.com>

--- src/app/components/user/user-from/user-form.component.ts
+++ src/app/components/user/user-from/user-form.component.ts
@@ -1,48 +1,54 @@
-import { Component, Input, inject } from '@angular/core';
-import { IFeedBackMessage, IUser, IFeedbackStatus} from '../../../interfaces';
-import { CommonModule } from '@angular/common';
-import { FormsModule, NgForm } from '@angular/forms';
-import { UserService } from '../../../services/user.service';
+import { Component, Input, inject } from "@angular/core";
+import { IFeedBackMessage, IUser, IFeedbackStatus } from "../../../interfaces";
+import { CommonModule } from "@angular/common";
+import { FormsModule, NgForm } from "@angular/forms";
+import { UserService } from "../../../services/user.service";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-  selector: 'app-user-form',
+  selector: "app-user-form",
   standalone: true,
-  imports: [
-    CommonModule, 
-    FormsModule
-  ],
-  templateUrl: './user-form.component.html',
-  styleUrl: './user-form.component.scss'
+  imports: [CommonModule, FormsModule],
+  templateUrl: "./user-form.component.html",
+  styleUrl: "./user-form.component.scss",
 })
 export class UserFormComponent {
   @Input() title!: string;
   @Input() user: IUser = {
-    email: '',
-    lastname: '',
-    password: '',
-    name: ''
+    email: "",
+    lastname: "",
+    password: "",
+    name: "",
   };
-  @Input() action: string = 'add'
+  @Input() action: string = "add";
   service = inject(UserService);
-  feedbackMessage: IFeedBackMessage = {type: IFeedbackStatus.default, message: ''};
+  feedbackMessage: IFeedBackMessage = {
+    message: "",
+  };
 
-  handleAction (form: NgForm) {
+  toastSvc = inject(ToastrService);
+
+  handleAction(form: NgForm) {
     if (form.invalid) {
-      Object.keys(form.controls).forEach(controlName => {
+      Object.keys(form.controls).forEach((controlName) => {
         form.controls[controlName].markAsTouched();
       });
       return;
     } else {
-      this.service[ this.action == 'add' ? 'saveUserSignal': 'updateUserSignal'](this.user).subscribe({
+      this.service[
+        this.action == "add" ? "saveUserSignal" : "updateUserSignal"
+      ](this.user).subscribe({
         next: () => {
-          this.feedbackMessage.type = IFeedbackStatus.success;
-          this.feedbackMessage.message = `User successfully ${this.action == 'add' ? 'added': 'updated'}`
+          this.feedbackMessage.message = `User successfully ${
+            this.action == "add" ? "added" : "updated"
+          }`;
+          this.toastSvc.success(this.feedbackMessage.message, "SUCCES!!!");
         },
         error: (error: any) => {
-          this.feedbackMessage.type = IFeedbackStatus.error;
           this.feedbackMessage.message = error.message;
-        }
-      })
+          this.toastSvc.error(this.feedbackMessage.message, "OH NO!");
+        },
+      });
     }
   }
 }
--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -24,7 +24,6 @@ export interface IAuthority {
 }
 
 export interface IFeedBackMessage {
-  type?: IFeedbackStatus;
   message?: string;
 }
 
