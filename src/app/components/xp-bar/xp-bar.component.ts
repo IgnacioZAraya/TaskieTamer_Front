6ad41f8fe47361590f0cd3190c6e1b85f53e3b41
DeibiU <dhuertasl@ucenfotec.ac.cr>
2024-07-24 03:11:19 -0600

    tailwind implementation and progressbar V1
    Co-authored-by: Ignacio Z A <IgnacioZelAra@users.noreply.github.com>

--- src/app/components/xp-bar/xp-bar.component.ts
+++ src/app/components/xp-bar/xp-bar.component.ts
@@ -1,26 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import gsap from 'gsap';
+
 
 @Component({
   selector: 'app-xp-bar',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './xp-bar.component.html',
   styleUrl: './xp-bar.component.scss'
 })
 export class XpBarComponent {
+  public progressPercentage!: string;
 
-  ngOnInit(): void{
-    this.animatedProgressBars();
+  public experience: number = 1250;
+  public valueLevel: number = 2500;
+
+  constructor() {
+    this.percentageCalc(this.experience, this.valueLevel);
   }
 
-  animatedProgressBars(){
-    gsap.to('#mainProgressBar', {
-      value: 90,
-      duration: 2,
-      ease: "power2.inOut"
-    });
-  }
+  private percentageCalc(dividend: number, divisor: number) : void{
+    let percentage = (dividend/divisor)*100;
 
+    this.progressPercentage = percentage.toString()+"%";
+  }
 }
