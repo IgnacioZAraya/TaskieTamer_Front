5bb269418f1ab385e2c609f7a3fea94905c63f9a
Ignacio Zelada <109553320+IgnacioZAraya@users.noreply.github.com>
2024-08-12 19:02:54 -0600

    Component Creation (Task Completion Page)

---
+++ src/app/pages/task-completion/task-completion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-completion',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-completion.component.html',
+  styleUrl: './task-completion.component.scss'
+})
+export class TaskCompletionComponent {
+
+}
